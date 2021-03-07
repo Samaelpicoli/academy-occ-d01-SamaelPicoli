b5ff7c75e488b880107c6a57a8f14f8db63c3f93
Samael Picoli <samaelmunizpicoli27@gmail.com>
2021-03-06 23:48:59 -0300

    occ

---
+++ widget/academy-occ-d01-SamaelPicoli/js/academy-occ-d01-SamaelPicoli.js
@@ -0,0 +1,11 @@
+define(['jquery', 'knockout'], function ($, ko) {
+  'use strict'
+
+  return {
+    onLoad: function (widget) {
+      console.log('static bootstrap 3 carousel')
+    },
+
+    beforeAppear: function () {}
+  }
+})
